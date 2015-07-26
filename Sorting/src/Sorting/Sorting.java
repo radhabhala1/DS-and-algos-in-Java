7c2a9d0053a6a08f543557a6f0fc03f370eb0fa6
rabhala@infiniteenergy.com <rabhala@infiniteenergy.com>
2015-07-26 16:44:46 -0400

    selesction and bubble

--- Sorting/src/Sorting/Sorting.java
+++ Sorting/src/Sorting/Sorting.java
@@ -3,33 +3,65 @@ package Sorting;
 public class Sorting {
 
     public static void main(String[] args) {
-        int[] array = {1, -1, 3,6,2,5,5};
-        array = insertionSort(array);
-        for(int a: array){
+        int[] array = {1, -1, 3, 6, 2, 5, 5};
+        //array = insertionSort(array);
+        //array = selectionSort(array);
+        array = bubbleSort(array);
+        for (int a : array) {
             System.out.print(a + " ");
         }
-        
+
     }
-    
-   public static int[] insertionSort(int[] array){
-       int length = array.length;
-       int temp = 0;
-       for(int i=1; i<length; i++){
-           for(int j=i; j>0; j--){
-              if(array[j]<array[j-1]){
-                  exchange(array,j,j-1);
-              } 
-           }
-       }
-       return array;
-   }
-   
-   public static void exchange(int[] array, int fIndex, int sIndex){
-       int temp = 0;
-       temp = array[fIndex];
-       array[fIndex]= array[sIndex];
-       array[sIndex] = temp;
-       
-   }
-    
+
+    public static int[] insertionSort(int[] array) {
+        int length = array.length;
+        for (int i = 1; i < length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    exchange(array, j, j - 1);
+                }
+            }
+        }
+        return array;
+    }
+
+    public static int[] selectionSort(int[] array) {
+        int length = array.length;
+        int i, j;
+        for (i = 0; i < length; i++) {
+            int min = i;
+            for (j = i + 1; j < length; j++) {
+                if (array[min] > array[j]) {
+                    min = j;
+
+                }
+            }
+            exchange(array, i, min);
+        }
+        return array;
+    }
+
+    public static int[] bubbleSort(int[] array) {
+        int length = array.length;
+        boolean swap = true;
+        while (swap) {
+            swap = false;
+            for (int i = 0; i < length - i; i++) {
+                if(array[i]>array[i+1]){
+                    exchange(array, i, i+1);
+                    swap = true;
+                }
+            }
+        }
+    return array;
+    }
+
+    public static void exchange(int[] array, int fIndex, int sIndex) {
+        int temp = 0;
+        temp = array[fIndex];
+        array[fIndex] = array[sIndex];
+        array[sIndex] = temp;
+
+    }
+
 }
